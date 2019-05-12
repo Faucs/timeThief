20b90198639e9e729e91a73f1a0807a17f9a4ab4
Magpie <cristian.gonzalezjo@sansano.usm.cl>
2019-05-12 01:02:54 -0400

    Subido player controller
    y otras weas del sabado 11 de mayo (aun no se corrige velocidad de movimiento, no deberia acelerar)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpf = 10;
+    public float travelTime = 1.6f;
+    public float screenSize = 14.0f;
+    private float currT = 0.0f;
+    private float posX = 0.0f;
+    private float posY = 0.0f;
+	// Use this for initialization
+	void Start () {
+        posX = this.transform.position.x;
+        posY = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {                                //una vez cada frame
+		/*/if(Input.GetKey(KeyCode.RightArrow))    //flecha derecha
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0), ForceMode2D.Impulse);
+            //aplicamos el torque en el eje z, en modo impulso (considerando la masa del RB)
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-force, 0), ForceMode2D.Impulse);
+        }*/
+        posX = this.transform.position.x;
+        posY = this.transform.position.y;
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+        // Update is called once per frame
+        /*if (currT >maxT)
+        {
+            Destroy(gameObject);
+        }*/
+        currT += (float) Time.deltaTime;
+        transform.position = new Vector2(currT*screenSize/travelTime + posX, posY); //debe demorar 1.6 seg entre el punto a y b v
+    }
+}
---
+++ Assets/Scripts/SingleBeat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleBeat : MonoBehaviour
+{
+    public float maxT = 0f;
+    public float travelTime = 1.6f;
+    private float currT = 0.0f;
+    private float distanceAB = 14.0f;
+    private float spawnPointX = 7.0f;
+    private float spawnPointY = 0.0f;
+    private float spawnPointZ = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currT >maxT)
+        {
+            Destroy(gameObject);
+        }
+        currT += (float) Time.deltaTime;
+        transform.localPosition = new Vector3(-currT*distanceAB/travelTime + spawnPointX, spawnPointY, spawnPointZ); //debe demorar 1.6 seg entre el punto a y b v
+    }
+}
