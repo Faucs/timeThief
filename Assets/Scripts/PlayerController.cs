7880f0a37f5fe6bed9d56271e4af5fb1bb6b53ec
Magpie <cristian.gonzalezjo@sansano.usm.cl>
2019-06-13 07:18:24 -0400

    Ver 0.01
    -TIMINNG FINALLY WORKS
    -Collisions now work through tilemaps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*El casatesoros corresponde actualmente a un sprite de 500*700 con pixeles 100 por unidad
+https://www.youtube.com/watch?v=dfe6LT9VCBc
+https://www.all8.com/tools/bpm.htm*/
 public class PlayerController : MonoBehaviour
 {
     public float jumpf = 10;
-    public float travelTime = 1.6f;
-    public float screenSize = 14.0f;
+    public float bpm = 0;
+    public float screenSize = 12.0f;
     private float currT = 0.0f;
     private float posX = 0.0f;
     private float posY = 0.0f;
-	// Use this for initialization
-	void Start () {
-        posX = this.transform.position.x;
-        posY = this.transform.position.y;
-	}
-	
-	// Update is called once per frame
-	void Update () {                                //una vez cada frame
+    private float jumpStart;
+    float speed;
+    void Start (){
+    }
+
+	void Update () {
 		/*/if(Input.GetKey(KeyCode.RightArrow))    //flecha derecha
         {
             this.GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0), ForceMode2D.Impulse);
@@ -27,18 +27,19 @@ public class PlayerController : MonoBehaviour
         {
             this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-force, 0), ForceMode2D.Impulse);
         }*/
+        speed = bpm/60;
         posX = this.transform.position.x;
         posY = this.transform.position.y;
+        
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
+            jumpStart = 0.0f;
             this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
         }
-        // Update is called once per frame
         /*if (currT >maxT)
         {
             Destroy(gameObject);
         }*/
-        currT += (float) Time.deltaTime;
-        transform.position = new Vector2(currT*screenSize/travelTime + posX, posY); //debe demorar 1.6 seg entre el punto a y b v
+        transform.position = new Vector2((Time.deltaTime*screenSize*speed) + posX, posY); //debe demorar 1.6 seg entre el punto a y b v
     }
 }
---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// The Audio Source component has an AudioClip option.  The audio
+// played in this example comes from AudioClip and is called audioData.
+[RequireComponent(typeof(AudioSource))]
+public class SoundTrigger : MonoBehaviour
+{
+    AudioSource audioData;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        //audioData.Play(0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        audioData.Play(0);
+    }
+}
